2dc4027afde638e2edf21d3e8b4c3282f636ce67
javierromualdo2006 <javieragustinromualdo2006@gmail.com>
2024-05-14 16:32:55 -0300

    ejercicio 5

---
+++ evaluacion /ejercicio 5.sql
@@ -0,0 +1,4 @@
+SELECT max(d.age) as 'edad maxima', min(d.age) as 'edad minima', avg(d.age) as 'edad promedio'
+FROM person p LEFT JOIN drivers_license d 
+ON p.license_id = d.id 
+WHERE d.gender IS 'female'
